f21b0d3bd6bcc83b634f38802e64dd3b488c25c7
Ponta <ponta12@naver.com>
2018-10-30 22:43:13 +0900

    Initial commit

---
+++ app/src/main/java/com/mycalendar/mycal/mycal/CalendarActivity.java
@@ -0,0 +1,13 @@
+package com.mycalendar.mycal.mycal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
